f934568b17014a1eb06941b9168919fa0a4c312a
YAZILIMCIABII <129683968+AhmetFarukTUNC@users.noreply.github.com>
2023-06-28 22:00:45 +0300

    ileri seviye fonksiyonlar
    ileri seviye fonksiyonlar

---
+++ ileri.js
@@ -0,0 +1,25 @@
+var selamfks = function selam(){
+    console.log("merhaba")
+}
+selamfks()
+
+const selamfks2=()=>{
+    console.log("merhaba2")
+}
+
+selamfks2()
+
+const selamfks3=()=>console.log("merhaba3")
+
+
+selamfks3()
+
+const mesaj = "merhaba4"
+
+const selamfks4=(mesaj)=>console.log(mesaj)
+
+selamfks4(mesaj)
+
+var topla=(sayı1,sayı2)=>{return sayı1+sayı2}
+
+console.log(topla(2,3))
